cbf92a1ccc2ccb468ba3885ca0cb750ed70348b0
Shubham432000 <shubham7410112997@gmail.com>
2022-11-07 19:37:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let data = document.getElementById("value1")
+
+function change(){
+    
+    let data1 = data.value
+    console.log(data1)
+    document.getElementById("show").textContent=data.value
+}
